6366a7373391290e3f729025cc51d7e517a86ab4
fei <rubyll0920@gmail.com>
2024-07-16 13:48:05 +0800

    change image path

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
+import { getImageURL } from "../utils/imageUtil";
 import { useTheme } from "./ThemeContext";
 
 const Footer = () => {
   const { darkMode } = useTheme();
-  let icon = darkMode ? "/pilipala/icon-white.svg" : "/pilipala/icon.svg";
+  let icon = darkMode ? "icon-white.svg" : "icon.svg";
   return (
     <footer className="sm: flex items-center justify-center border-t border-gray-200 px-3 py-[8px] sm:border-none sm:py-[24px] dark:border-gray-500">
-      <img src={icon} alt="" className="mr-2 inline-block w-6" />
+      <img src={getImageURL(icon)} alt="" className="mr-2 inline-block w-6" />
       <span className="pr-3">©Tingyu Liu 2024</span>
     </footer>
   );
